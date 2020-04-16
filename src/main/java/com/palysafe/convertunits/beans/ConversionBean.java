c049bfdc547c49e667ff2196f95dbf0249ab93d3
Gumbo-MberiM <gumbo-mberim@agb.rbxd.ds>
2020-04-16 13:12:52 +0200

    third commit : created conversion bean

---
+++ src/main/java/com/palysafe/convertunits/beans/ConversionBean.java
@@ -0,0 +1,63 @@
+package com.palysafe.convertunits.beans;
+
+
+/**
+ * This is a bean used to convert the values
+ */
+public class ConversionBean {
+
+    private String convertFrom;
+    private String convertTo;
+    private Double convertValue;
+    private Double convertedValue;
+    private String message;
+    private String timeTaken;
+
+    public String getConvertFrom() {
+        return convertFrom;
+    }
+
+    public void setConvertFrom(String convertFrom) {
+        this.convertFrom = convertFrom;
+    }
+
+    public String getConvertTo() {
+        return convertTo;
+    }
+
+    public void setConvertTo(String convertTo) {
+        this.convertTo = convertTo;
+    }
+
+    public Double getConvertValue() {
+        return convertValue;
+    }
+
+    public void setConvertValue(Double convertValue) {
+        this.convertValue = convertValue;
+    }
+
+    public Double getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(Double convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimeTaken() {
+        return timeTaken;
+    }
+
+    public void setTimeTaken(String timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+}
