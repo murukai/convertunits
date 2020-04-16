5e15b6917ae7be0e2e08aef7eddef096690fe541
Gumbo-MberiM <gumbo-mberim@agb.rbxd.ds>
2020-04-16 14:12:44 +0200

    tenth commit : bad requests

--- src/main/java/com/palysafe/convertunits/rest/ConversionController.java
+++ src/main/java/com/palysafe/convertunits/rest/ConversionController.java
@@ -18,26 +18,16 @@ public class ConversionController {
 
     @PostMapping("/ktom")
     public ResponseEntity<ConversionBean> convertKilometers(@Valid @RequestBody ConversionBean conversionBean){
-        StringBuffer stringBuffer = new StringBuffer();
-        if(conversionBean.getConvertFrom().equalsIgnoreCase("celcius") || conversionBean.getConvertFrom().equalsIgnoreCase("celcius")){
-            conversionBean.setMessage("Bad request please try temperature requests");
-            return ResponseEntity.status(400)
-                    .body(conversionBean);
-        }
-        conversionBean = conversionService.convertDistance(conversionBean);
-
-        stringBuffer.append(conversionBean.getConvertValue() + " " + conversionBean.getConvertFrom() + " = " + conversionBean.getConvertedValue() + " " + conversionBean.getConvertTo());
-
-
-        conversionBean.setMessage(stringBuffer.toString());
-
-        return ResponseEntity.status(200)
-                .body(conversionBean);
+        return getDistanceConversionBeanResponseEntity(conversionBean);
     }
 
 
     @PostMapping("/mtok")
     public ResponseEntity<ConversionBean> convertMiles(@Valid @RequestBody ConversionBean conversionBean){
+        return getDistanceConversionBeanResponseEntity(conversionBean);
+    }
+
+    private ResponseEntity<ConversionBean> getDistanceConversionBeanResponseEntity(@RequestBody @Valid ConversionBean conversionBean) {
         StringBuffer stringBuffer = new StringBuffer();
 
         if(conversionBean.getConvertFrom().equalsIgnoreCase("celcius") || conversionBean.getConvertFrom().equalsIgnoreCase("celcius")){
@@ -57,6 +47,10 @@ public class ConversionController {
 
     @PostMapping("/ktoc")
     public ResponseEntity<ConversionBean> convertKelvins(@Valid @RequestBody ConversionBean conversionBean){
+        return getTemperatureConversionBeanResponseEntity(conversionBean);
+    }
+
+    private ResponseEntity<ConversionBean> getTemperatureConversionBeanResponseEntity(@RequestBody @Valid ConversionBean conversionBean) {
         StringBuffer stringBuffer = new StringBuffer();
 
         if(conversionBean.getConvertFrom().equalsIgnoreCase("KM") || conversionBean.getConvertFrom().equalsIgnoreCase("MI")){
@@ -75,25 +69,9 @@ public class ConversionController {
     }
 
 
-
     @PostMapping("/ctok")
     public ResponseEntity<ConversionBean> convertCelcius(@Valid @RequestBody ConversionBean conversionBean){
-        StringBuffer stringBuffer = new StringBuffer();
-
-        if(conversionBean.getConvertFrom().equalsIgnoreCase("KM") || conversionBean.getConvertFrom().equalsIgnoreCase("MI")){
-            conversionBean.setMessage("Bad request please try temperature requests");
-            return ResponseEntity.status(400)
-                    .body(conversionBean);
-        }
-
-        conversionBean = conversionService.convertTemperature(conversionBean);
-
-        stringBuffer.append(conversionBean.getConvertValue() + " degrees " + conversionBean.getConvertFrom() + " = " + conversionBean.getConvertedValue() +  " degrees " + conversionBean.getConvertTo());
-
-        conversionBean.setMessage(stringBuffer.toString());
-
-        return ResponseEntity.status(200)
-                .body(conversionBean);
+        return getTemperatureConversionBeanResponseEntity(conversionBean);
     }
 
 
