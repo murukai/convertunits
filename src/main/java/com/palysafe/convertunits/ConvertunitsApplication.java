ba0945d3091878d91a41fbeb96ad83c48b262660
Gumbo-MberiM <gumbo-mberim@agb.rbxd.ds>
2020-04-16 13:06:27 +0200

    second commit : created the project

---
+++ src/main/java/com/palysafe/convertunits/ConvertunitsApplication.java
@@ -0,0 +1,13 @@
+package com.palysafe.convertunits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertunitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertunitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/palysafe/convertunits/ConvertunitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palysafe.convertunits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertunitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
